33ccdec5a6dff8cb1ccb357a983706dc7d98cbf4
任丰 <renfeng@kingchannels.com>
2022-05-20 20:40:54 +0800

    添加首页空页面

---
+++ src/page/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="index">
+    index
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "index",
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="less">
+#index{
+  
+}
+</style>
